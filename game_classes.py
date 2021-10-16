6a2da4b2a8e326165e0e8c9602cf018719f6b435
KakeinNoriaki <92641306+KakeinNoriaki@users.noreply.github.com>
2021-10-16 20:54:50 +0300

    Create game_classes.py

---
+++ game_classes.py
@@ -0,0 +1,30 @@
+class Player:
+    def __init__(self, hp, mp, wp):
+        self.hit_point_max = 100
+        self.hit_poin = hp
+        self.mana_point_max = 100
+        self.mana_point = mp
+        self.weapon_in_hands = wp
+        self.weapon_inventory = []
+
+
+
+class Enemy:
+   pass
+
+
+class Weapon:
+    pass
+
+
+class Armor:
+    pass
+
+
+class Artefact:
+    pass
+
+
+class Potion:
+    pass
+
