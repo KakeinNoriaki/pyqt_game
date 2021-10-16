fec483643867869f47a12f4496f0ac8d7daae7e1
KakeinNoriaki <maks.knyazev.05@yandex.ru>
2021-10-16 21:36:58 +0300

    Update game_classes.py

--- game_classes.py
+++ game_classes.py
@@ -1,30 +1,48 @@
 class Player:
-    def __init__(self, hp, mp, wp):
+    def __init__(self, hp, mp, wp, ar=None, arteff=None):
         self.hit_point_max = 100
         self.hit_poin = hp
         self.mana_point_max = 100
         self.mana_point = mp
         self.weapon_in_hands = wp
         self.weapon_inventory = []
-
+        self.armor = ar
+        self.artefact_effect = arteff
+        self.potion_effect = None
 
 
 class Enemy:
-   pass
+    def __init__(self, nm, hp, dm):
+        self.name = nm
+        self.hit_points = hp
+        self.damage = dm
+        self.pattern = []
+        self.next_move = None
 
 
 class Weapon:
-    pass
+    def __init__(self, nm, dm):
+        self.name = nm
+        self.damage = dm
 
 
 class Armor:
-    pass
+    def __init__(self, nm, php, pmp):
+        self.name = nm
+        self.plus_hit_point = php
+        self.plus_mana_point = pmp
 
 
 class Artefact:
-    pass
+    def __init__(self, nm, pd, ef):
+        self.name = nm
+        self.plus_damage = pd
+        self.effect = ef
 
 
 class Potion:
-    pass
+    def __init__(self, nm, hph, ef):
+        self.name = nm
+        self.hit_point_heall = hph
+        self.effect = ef
 
